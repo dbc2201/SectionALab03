1930aacfb637211d537d27023414c8a9d0b88c7d
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-08-26 14:53:14 +0530

    methods

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,15 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("inside parantha branch");
+		String stringClassLaObject = "Divyansh";
+		boolean boolVariable = stringClassLaObject.equals("Hello");
+		int l = stringClassLaObject.length();
+		System.out.println(l);
+		char[] individualCharacters = stringClassLaObject.toCharArray();
+		char c = stringClassLaObject.charAt(0);
+		stringClassLaObject.concat(" BHardwaj");
+		stringClassLaObject.indexOf('a');
+		stringClassLaObject.isEmpty();
+		String[] s = stringClassLaObject.split(" ");
 	}
 }
